6bb164244d8ee073c6be5966ebcf25255c9f068a
Orion Delwaterman <orion.delwaterman@tollbit.com>
2025-12-10 15:08:30 -0500

    Refactor client

--- examples/self_reporting.py
+++ examples/self_reporting.py
@@ -15,23 +15,27 @@ user_agent = os.getenv("TOLLBIT_USER_AGENT", "tollbit-python-sdk-example/0.1.0")
 
 reporting_client = self_reporting.create_client(secret_key=api_key, user_agent=user_agent)
 
-block = self_reporting.create_transaction_block()
-block.add_usage(
+usages = []
+usages.append(self_reporting.usage(
     url="https://pioneervalleygazette.com/daydream",
     times_used=1,
     license_permissions=[licences.permission.LICENSE_PERMISSION_PARTIAL_USE],
     license_cuid="license-cuid-example-1234",
     license_type=licences.ON_DEMAND_LICENSE,
-)
-block.add_usage(
+))
+
+usages.append(self_reporting.usage(
     url="https://pioneervalleygazette.com/sunset",
     times_used=2,
     license_permissions=[licenses.permission.LICENSE_PERMISSION_FULL_USE, licenses.permission.LICENSE_PERMISSION_CITATION_REQURED],
     license_cuid="license-cuid-example-5678",
     license_type=licences.type.ON_DEMAND_FULL_USE_LICENSE,
     metadata={"another_key": "another_value"},
-)
+))
 
-result = reporting_client.submit_transaction_block(block)
+# We create a transaction here that is idempotent, so that every
+# reporting request can be safely multiple times.
+tb = reporting_client.create_transaction_block(usages)
+result = reporting_client.record(tb)
 
 print("Transaction result:", result)
