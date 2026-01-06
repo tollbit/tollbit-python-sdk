840af9f758a26b765d663464a88e8cda5aac2c73
Orion Delwaterman <orion.delwaterman@tollbit.com>
2026-01-06 10:44:36 -0500

    Fix formatting

--- src/tollbit/self_reporting/__init__.py
+++ src/tollbit/self_reporting/__init__.py
@@ -1,2 +1,7 @@
-from .client import create_client, create_async_client, SelfReportingClient, AsyncSelfReportingClient
+from .client import (
+    create_client,
+    create_async_client,
+    SelfReportingClient,
+    AsyncSelfReportingClient,
+)
 from .usage import usage
