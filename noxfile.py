c8234a72bc5447859fe98ee5c79c7873dda5285f
Orion Delwaterman <orion.delwaterman@tollbit.com>
2026-01-07 09:59:41 -0500

    Fix nox tests

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,5 @@ import nox
 @nox.session(python=["3.12", "3.11", "3.10"])
 def tests(session):
     """Run pytest for all supported Python versions."""
-    session.install("pytest")
-    session.install(".")        # install the package from pyproject.toml
+    session.install(".[dev]")  # install main and dev dependencies from pyproject.toml
     session.run("pytest", "-q")
